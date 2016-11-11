5c1663f4193862948a5fd69400e8b708488016d5
Erick <github@ericklavoie.com>
2016-11-11 14:28:58 -0500

    Fixed bug

--- src/work.js
+++ src/work.js
@@ -1,8 +1,7 @@
 var pull = require('pull-stream')
-var pando = require(__dirname)
 
 module.exports = function (cb) {
-    pando.node().then((node) => {
+    this.node().then((node) => {
         node._libp2p.dialByMultiaddr(
             '/ip4/192.168.0.174/tcp/4002/ipfs/QmP4gHA2TjXuXjZ2qNk5T7eXB91aeZuWDQM7sUeiAF3yJE',
             '/jobs/1.0.0',
