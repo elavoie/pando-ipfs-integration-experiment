b429aad0adc130670b58c829b26886e708bcd371
Erick <github@ericklavoie.com>
2016-11-18 19:32:10 -0500

    WIP

--- src/index.js
+++ src/index.js
@@ -1,43 +1,15 @@
-var start = require(__dirname + '/start.js')
-var work = require(__dirname + '/work.js')
-var promisify = require(__dirname + '/promisify.js')
+var Node = require('./node.js')
 
-function saveNode (start) {
-    return function (options, cb) {
-        start(options, function (err, node) {
-            if (err) return cb(err)
+/*
 
-            pando._node = node
-            cb(null, node)
-        }) 
-    } 
+function multiaddrs (node) {
+  console.log('Pando node listening on:')
+  node._libp2p.peerInfo.multiaddrs.forEach((a) => {
+    console.log(a.toString() + '/ipfs/' + node._libp2p.peerInfo.id.toB58String())
+  })
 }
 
-function node (cb) {
-    if (pando._node === null) {
-        pando.start((err, node) => {
-            if (err) return cb(err)
+pando.multiaddrs = multiaddrs
+*/
 
-            cb(null, node)
-        })
-    }
-    cb(null, pando._node)
-}
-
-function addresses (node) {
-    console.log('Pando node listening on:')
-    node._libp2p.peerInfo.multiaddrs.forEach((a) => {
-        console.log(a.toString() + '/ipfs/' + node._libp2p.peerInfo.id.toB58String()) 
-    })
-}
-
-var pando = promisify({
-    _node: null,
-    node: node,
-    start: saveNode(start),
-    work: work
-})
-
-pando.addresses = addresses
-
-module.exports = pando
+module.exports.Node = Node
--- src/start.js
+++ src/start.js
@@ -1,162 +1,107 @@
-var path = require('path')
 var fs = require('fs')
 var IPFSApi = require('ipfs-api')
-var Libp2p = require('libp2p-ipfs')
+var libp2p = require('libp2p-ipfs')
 var PeerId = require('peer-id')
 var PeerInfo = require('peer-info')
 var multiaddr = require('multiaddr')
-var ip = require('ip')
-var pull = require('pull-stream')
-var toPull = require('pull-promise')
-var promisify = require(__dirname + '/promisify')['function']
-var net = require('net');
+var pify = require('pify')
+var debug = require('debug')
+var log = debug('pando:start')
+log.error = debug('pando:start:error')
+var protocols = require('./protocols')
+var utils = require('./utils')
+var path = require('path')
 
-function getUserHome() {
-  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
-}
+var createPeerInfo = pify(function createPeerInfo (config, cb) {
+  log('retrieving peerid')
+  PeerId.createFromPrivKey(config.Identity.PrivKey, (err, peerId) => {
+    if (err) {
+      log.err(err)
+      return cb(err)
+    }
 
-// libp2p is used for communication between nodes within a dedicated for Pando nodes
-// ipfs is used for storing and retrieving data
-function Node(libp2p, ipfs) {
-    this._libp2p = libp2p
-    this._ipfs = ipfs
-}
-
-var usePortIfFree = promisify(function (port, cb) {
-    var server = net.createServer();
-    server.on('listening', function() {
-        port = server.address().port
-        server.close()
-    })
-    server.on('close', function() {
-        cb(null, port)
-    })
-    server.on('error', function(err) {
-        console.log('error: ' + err);
-        cb(null, 0)
-    })
-    server.listen(port)
+    log('retrieved id: ' + peerId.toB58String())
+    var peerInfo = new PeerInfo(peerId)
+    cb(null, peerInfo)
+  })
 })
 
-var createPeerInfo = promisify(function createPeerInfo(config, cb) {
-    if (options.verbose) { console.log('Retrieving PeerId') }
-    PeerId.createFromPrivKey(config.Identity.PrivKey, (err, peerId) => {
-        if (err) return cb(err)
+var startNetwork = pify(function startNetwork (node, cb) {
+  log('Starting communication node')
+  node._libp2p.start((err) => {
+    if (err) {
+      log.error(err)
+      return cb(err)
+    }
 
-        if (options.verbose) { console.log('Retrieved id: ' + peerId.toB58String()) }
-        var peerInfo = new PeerInfo(peerId)
-        cb(null, peerInfo)
-    })
+    log('Node started successfully')
+
+    log('Installing protocols')
+    protocols.install(node, '/jobs/1.0.0')
+
+    node._started = true
+
+    log('Node listening')
+    node._events.emit('listening')
+
+    cb(null, node)
+  })
 })
 
-var addProtocols = function addProtocols(network) {
-    if (options.verbose) { console.log('Adding pando protocols') }
+module.exports = function (node, cb) {
+  if (typeof cb !== 'function') {
+    throw new Error('Invalid callback function')
+  }
 
-    var jobs = pull(pull.count(), pull.map(String))
-    network.handle('/jobs/1.0.0', (protocol, conn) => {
-        console.log('protocol: ' + protocol)
-        pull(
-            toPull.source(new Promise((resolve, reject) => {
-                pull(
-                    jobs,
-                    pull.take(1),
-                    pull.drain((j) => resolve(j), () => reject(true))
-                )
-            })),
-            pull.through(console.log),
-            conn,
-            pull.drain((x) => { console.log('result: ' + x.toString()) }, 
-                       (err) => { console.log('error: ' + err) })
-        )
-    })
-}
+  // We need the private key to create the peer Id but the API does not provide it
+  // so we are retrieving it directly from the config file. The node shares
+  // the same id on both the IPFS and Pando (libp2p) networks
+  //
+  // Assumption:
+  // - ipfs init has been run previously to initialize the ipfs repo
+  try {
+    log('Reading config')
+    var config = JSON.parse(fs.readFileSync(path.join(node._options.repoPath, 'config')))
+  } catch (err) {
+    log.error(err)
+    cb(err)
+  }
 
-var startNetwork = promisify(function startNetwork(network, cb) {
-    if (options.verbose) { console.log('Starting communication node') }
-    network.start((err) => {
-        if (err) cb(err)
-        addProtocols(network)
+  // Connecting to ipfs daemon
+  utils.isIpfsRunning((running) => {
+    if (!running) {
+      var err = new Error('ipfs not running, aborting')
+      log.error(err)
+      return cb(err)
+    }
 
-        if (options.verbose) { console.log('Communication node ready, listening on:') }
-        network.peerInfo.multiaddrs.forEach((ma) => {
-            console.log(ma.toString() + '/ipfs/' + network.peerInfo.id.toB58String())
-        })
+    try {
+      log('Starting ipfs api')
+      node._ipfs = new IPFSApi()
+    } catch (err) {
+      log.error(err)
+      cb(err)
+    }
+  })
 
-        cb(null, network)
-    })
-})
+  // We need to create a complete peer info (using the peer id) for using the
+  // lower-level apis of libp2p
+  createPeerInfo(config)
+    .then((peerInfo) => {
+      log('Creating communication node')
+      node._libp2p = new libp2p.Node(peerInfo)
 
-var addMultiaddr = promisify(function addMultiaddr(peerInfo, port, cb) {
-    // Let the OS pick the current network address
-    peerInfo.multiaddr.add(multiaddr.fromNodeAddress({
+      log('Adding multiaddresses')
+      node._libp2p.peerInfo.multiaddr.add(multiaddr.fromNodeAddress({
         family: 'IPv4',
         address: '0.0.0.0',
-        port: port
-    }, 'tcp'))
+        port: node._options.port
+      }, 'tcp'))
 
-    cb(null, peerInfo)
-})
-
-var createNetwork = promisify(function createNetwork(peerInfo, cb) {
-    if (options.verbose) { console.log('Creating communication node') }
-    var net = new Libp2p.Node(peerInfo)
-    cb(null, net)
-})
-
-var options = {
-    verbose: true
-}
-
-// Assumptions:
-// - ipfs daemon with the go-client is running
-// - ipfs init has been run previously to initialize an ipfs repo
-module.exports = function (opts, cb) {
-    if (cb === undefined) {
-        cb = opts
-    } else {
-        if (opts.hasOwnProperty('verbose')) {
-            options.verbose = opts.verbose
-        }
-    }
-
-    if (opts === undefined) {
-        throw new Error('Invalid callback function')
-    }
-
-    // This first section uses the go implementation (1) to obtain an identity
-    // and network addresses, and (2) to publish the supported
-    // protocols until js-ipfs supports enough features to be used directly
-    // both for data and communication
-    try {
-        // We need the private key to create the peer Id but the API does not provide it
-        // so we are retrieving it directly from the config
-        if (options.verbose) { console.log('Reading config') }
-        var config = JSON.parse(fs.readFileSync(path.join(getUserHome(), '.ipfs', 'config')))
-
-        // Initialize ipfs and obtain network addresses (including the public IP
-        // for nodes behind a NAT)
-        if (options.verbose) { console.log('Starting ipfs api') }
-        var ipfs = new IPFSApi()
-
-        // We need to create a complete peer info (using the peer id) for using the
-        // lower-level apis of libp2p for direct connection between nodes
-        createPeerInfo(config)
-        .then((peerInfo) => { 
-            return Promise.resolve(4002)
-            .then(usePortIfFree)
-            .then((port) => {
-                console.log('port: ' + port)
-                return addMultiaddr(peerInfo, port)
-            })
-            .then(createNetwork)
-            .then(startNetwork)
-            .then((network) => {
-                if (options.verbose) { console.log('Creating pando node') }
-                return cb(null, new Node(network, ipfs))
-            })
-            .catch(cb)
-         }).catch(cb)
-    } catch (err) {
-        cb(err)
-    }
+      startNetwork(node, cb)
+    })
+    .catch((err) => {
+      log.error(err)
+      cb(err)
+    })
 }
