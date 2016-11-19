67c2b2feb88c9187a94f3908cc3824d1c6c76396
Erick <github@ericklavoie.com>
2016-11-18 19:54:56 -0500

    Readded printing of concurrent jobs

--- test/spawn-node.js
+++ test/spawn-node.js
@@ -19,12 +19,14 @@ pull(
     })
 )
 
+/*
 node.onListening(() => {
   var node = new pando.Node({port: 4003}).start(() => {
     node.volunteer(
       '/ip4/127.0.0.1/tcp/4002/ipfs/QmP4gHA2TjXuXjZ2qNk5T7eXB91aeZuWDQM7sUeiAF3yJE')
   })
 })
+*/
 
 node.onJob(() => {
   console.log('nb of concurrent jobs: ' + node._jobs.length)
