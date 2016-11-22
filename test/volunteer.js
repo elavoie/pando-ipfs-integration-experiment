f3ad7042a5b83f81e1cb3c8503702098a544edfe
Erick <github@ericklavoie.com>
2016-11-22 17:20:08 -0500

    Added option to pass server address on the commandline

--- test/volunteer.js
+++ test/volunteer.js
@@ -1,5 +1,5 @@
 var pando = require('../')
 var node = new pando.Node({port: 0})
 node.start(() => {
-  node.volunteer('/ip4/192.168.0.174/tcp/4002/ipfs/QmP4gHA2TjXuXjZ2qNk5T7eXB91aeZuWDQM7sUeiAF3yJE', null, (err) => { if (err) throw err; else process.exit(0) })
+  node.volunteer(process.argv[2] || '/ip4/192.168.0.174/tcp/4002/ipfs/QmP4gHA2TjXuXjZ2qNk5T7eXB91aeZuWDQM7sUeiAF3yJE', null, (err) => { if (err) throw err; else process.exit(0) })
 })
