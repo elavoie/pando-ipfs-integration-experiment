be26e2154d3a6f1f3e4c43b6e801793f57afcb2d
Erick Lavoie <elavoie@freims>
2016-11-22 23:21:04 +0100

    WIP

--- tools/test-g5k.py
+++ tools/test-g5k.py
@@ -8,8 +8,9 @@ print 'Submitting job request'
     (execo_g5k.OarSubmission(resources="nodes=2"), "grenoble")
 ])
 
-server_cmd = 'source ~/.bashrc && cd pando.js && node test/spawn-node.js'
-worker_cmd = 'source ~/.bashrc && cd pando.js && node test/volunteer.js %s'
+server_cmd = 'cd pando.js && node test/spawn-node.js'
+worker_cmd = 'cd pando.js && node test/volunteer.js %s'
+params = execo_g5k.default_oarsh_oarcp_params
 
 if jobid:
     try:
@@ -17,22 +18,29 @@ if jobid:
         execo_g5k.wait_oar_job_start(jobid, site)
         print 'Retrieving nodes'
         nodes = execo_g5k.get_oar_job_nodes(jobid, site)
-        if (len(nodes) > 2):
+	print nodes
+        if (len(nodes) >= 2):
             server = execo.Remote(
                     server_cmd,
                     nodes[0],
-                    connection_params=execo_g5k.default_oarsh_oarcp_params)
+                    connection_params=params)
             with server.start():
                 execo.sleep(0.5)
-                (p, i, m) = server.expect(r'^(\/ip4\/172.*)')
-                execo.sleep(1)
-                print m
-                # workers = execo.Remote(
-                #        worker_cmd,
-                #        nodes[1:],
-                #        connection_params=execo_g5k.default_oarsh_oarcp_params)
-                print execo.Report([server]).to_string()
-                print server.processes.stdout
+                (h,i,m) = server.expect(r'^(\/ip4\/172.*)')[0]
+                multiaddr = m.group()
+		print 'Starting workers with cmd: ' + worker_cmd%(multiaddr)
+                workers = execo.Remote(
+                        worker_cmd%(multiaddr),
+                        nodes[1:],
+                        connection_params=params)
+		workers.expect('Node ready')
+		print 'Workers ready'
+		print 'Started processing'
+		server.expect('done')
+		print 'Processing done'
+                print execo.Report([server, workers]).to_string()
+		for index,p in enumerate(server):
+                    print p.stdout
         else:
             print 'Insufficient number of nodes'
 
